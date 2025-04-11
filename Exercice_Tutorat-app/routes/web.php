a7c69dc2934747da143666a8d38166c355b186de
Martin-L-tech <m_lemay@hotmail.com>
2025-04-11 10:45:35 -0400

    Creation de la première route

--- Exercice_Tutorat-app/routes/web.php
+++ Exercice_Tutorat-app/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $eleves=[
+        'matricule'=>'123141414',
+        'prenom'=>'Adam',
+        'nom'=>'Bernard'
+    ];
+return view('simulationConnection', ['eleves'=>$eleves]);
 });
+
